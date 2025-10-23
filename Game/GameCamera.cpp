2ebc4c036ae25f7de9090d9d8f1b1c132da4e1f4
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2025-10-23 18:30:13 +0900

    ゲームカメラ::<ばねカメラ実装>

--- Game/GameCamera.cpp
+++ Game/GameCamera.cpp
@@ -3,12 +3,20 @@
 #include "Player.h"
 
 
+void GameCamera::SetCamera(const CameraInfo& camera) {
+	m_toCameraPos.Set(camera.toCameraPos);
+	m_springCamera.Init(
+		*g_camera3D,
+		camera.maxCameraSpeed,
+		true,
+		camera.cameraCollisionScl
+		);
+}
+
+
 GameCamera::GameCamera() {
 	m_player = FindGO<Player>("player");
-
-	m_toCameraPos.Set(0.0f, 125.0f, -125.0f);
-	g_camera3D->SetNear(1.0f);
-	g_camera3D->SetFar(10000.0f);
+	SetCamera(cameraInfo);
 }
 
 
@@ -20,6 +28,7 @@ GameCamera::~GameCamera() {
 void GameCamera::Update() {
 	Vector3 target = m_player->m_position;
 	target.y += 80.0f;
+	target += g_camera3D->GetForward() * 20.0f;
 
 	Vector3 toCameraPosOld = m_toCameraPos;
 
@@ -28,30 +37,30 @@ void GameCamera::Update() {
 	stickR.y = g_pad[0]->GetRStickYF();
 
 	//YŽ²Žü‚è‚Ì‰ñ“]
-	Quaternion qRot = { Quaternion::Identity };
-	qRot.SetRotationDeg(Vector3::AxisY, 1.3f * stickR.x);
+	Quaternion qRot = Quaternion::Identity;
+	qRot.SetRotationDeg(Vector3::AxisY, 2.0f * stickR.x);
 	qRot.Apply(m_toCameraPos);
 
 	//XŽ²Žü‚è‚Ì‰ñ“]
 	Vector3 axisX = { Vector3::Zero };
 	axisX.Cross(Vector3::AxisY, m_toCameraPos);
 	axisX.Normalize();
-	qRot.SetRotationDeg(axisX, 1.3f * stickR.y);
+	qRot.SetRotationDeg(axisX, 2.0f * stickR.y);
 	qRot.Apply(m_toCameraPos);
 	Vector3 toPosDir = m_toCameraPos;
 	toPosDir.Normalize();
 	
 	
-	if (toPosDir.y < -0.2f) {
+	if (toPosDir.y < -0.5f) {
 		m_toCameraPos = toCameraPosOld;
 	}
-	else if (toPosDir.y > 0.9f) {
+	else if (toPosDir.y > 0.8f) {
 		m_toCameraPos = toCameraPosOld;
 	}
 
 	Vector3 pos = target + m_toCameraPos;
 
-	g_camera3D->SetTarget(target);
-	g_camera3D->SetPosition(pos);
-	g_camera3D->Update();
+	m_springCamera.SetTarget(target);
+	m_springCamera.SetPosition(pos);
+	m_springCamera.Update();
 }
--- Game/GameCamera.h
+++ Game/GameCamera.h
@@ -1,4 +1,21 @@
 #pragma once
+#include "camera/SpringCamera.h"
+
+
+
+namespace {
+	struct CameraInfo {
+		Vector3 toCameraPos;
+		float cameraRotateSpeed;
+		float maxCameraSpeed;
+		float cameraCollisionScl;
+	};	
+
+	const CameraInfo cameraInfo = {
+		{0.0f, 125.0f, -125.0f}, 2.0f, 1000.0f, 5.0f
+	};
+}
+
 class Player;
 
 class GameCamera : public IGameObject{
@@ -8,7 +25,13 @@ public:
 	void Update()override;
 	
 private:
+	void SetCamera(const CameraInfo& camera);
+
+private:
+	
+
 	Player* m_player = nullptr;
-	Vector3 m_toCameraPos = { Vector3::Zero };
+	Vector3 m_toCameraPos = Vector3::Zero;
+	SpringCamera m_springCamera;
 };
 
