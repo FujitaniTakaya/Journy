aa38fc41d7f1dd4934a15ecba0ce8df1a7a27839
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2025-11-18 12:46:51 +0900

    レベルでのfirstStage呼び出し処理追加

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -7,9 +7,44 @@
 
 bool Game::Start()
 {
-	m_stage = NewGO<Stage>(0, "stage");
-	m_player = NewGO<Player>(0, "player");
+	m_stageLevelRender.Init("Assets/level/attachEnemy.tkl", [&](LevelObjectData& objData) {
+		if (objData.EqualObjectName(L"unityChan")) {
+			m_player = NewGO<Player>(0, "player");
+			m_player->SetTRS(objData.position,objData.rotation,objData.scale);
+			return true;
+		}
+		if (objData.EqualObjectName(L"stageGround")) {
+			m_firstStage = NewGO<First>(0);
+			m_firstStage->SetTRS(objData.position, objData.rotation, objData.scale);
+			return true;
+		}
+		if (objData.EqualObjectName(L"goalPole")) {
+			m_goalPole = NewGO<GoalPole>(0);
+			m_goalPole->SetTRS(objData.position, objData.rotation, objData.scale);
+			return true;
+		}
+		if (objData.EqualObjectName(L"normalEnemy")) {
+			for (auto enemy : m_firstStageEnemies) {
+				enemy = NewGO<Normal>(0, "enemy");
+				enemy->SetFirstPosition(objData.position);
+				enemy->SetTRS(objData.position, objData.rotation, objData.scale);
+			}
+			return true;
+		}
+
+	});
+	
 	m_gameCamera = NewGO<GameCamera>(0, "gameCamera");
-	m_enemy = NewGO<Normal>(0, "normalEnemy");
+	DeleteGO(m_skyCube);
+
+	m_skyCube = NewGO<SkyCube>(5, "skyCube");
+	m_skyCube->SetType(static_cast<EnSkyCubeType>(m_skyCubeType));
+	m_skyCube->SetScale(5000.0f);
+	g_renderingEngine->SetAmbientByIBLTexture(m_skyCube->GetTextureFilePath(),1.0f);
 	return true;
+}
+
+
+void Game::Render(RenderContext& rc) {
+	m_stageLevelRender.Draw(rc);
 }
--- Game/Game.h
+++ Game/Game.h
@@ -1,6 +1,7 @@
 #pragma once
+#include "Level3DRender/LevelRender.h"
 
-//#include "Level3DRender/LevelRender.h"
+
 
 class Stage;
 class Player;
@@ -13,11 +14,18 @@ public:
 	Game() {}
 	~Game() {}
 	bool Start()override;
+	void Render(RenderContext& rc)override;
 
-private:
-	Stage* m_stage = nullptr;
+private:/*
+	Stage* m_firstStage = nullptr;
+	Stage* m_goalPole = nullptr;
+	std::array<Enemy*, 6> m_firstStageEnemies = { nullptr };
+	int m_floorCount = 0;
 	Player* m_player = nullptr;
 	GameCamera* m_gameCamera = nullptr;
-	Enemy* m_enemy = nullptr;
+	SkyCube* m_skyCube = nullptr;
+	int m_skyCubeType = enSkyCubeType_Wild_2;
+	LevelRender m_stageLevelRender;
+	//Enemy* m_enemy = nullptr;
 };
 
