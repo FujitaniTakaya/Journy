fb609f61375326e0a6d4ab951a81b4a11c8148a0
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2025-11-19 21:28:58 +0900

    不要になった関数、変数等の処理を削除

--- Game/UI.cpp
+++ Game/UI.cpp
@@ -7,31 +7,16 @@ bool UI::Start() {
 	m_player = FindGO<Player>("player");
 
 	for (int i = 0; i < UIInfo::Life::MAX_HP; i++) {
-		SpriteRender* lifePtr = &m_life[i].spriteRender;
 		lifePtr->Init("Assets/UI/life/life.dds", 50.0f, 50.0f);
 		lifePtr->SetPosition({ -700.0f - (i * 40.0f), 400.0f, 0.0f });
 		lifePtr->Update();
 	}
 
-	UpdateLife();
-
-
-	UpdateTimerText();
-	m_timerFontRender.SetText(m_timerText.data());
-	m_timerFontRender.SetPosition({ 600.0f, 40.0f, 0.0f });
-
-
-	
-	
 	return true;
 }
 
 
 void UI::Update() {
-	m_gameTimer += g_gameTime->GetFrameDeltaTime();
-	UpdateSecond();
-	UpdateMinute();
-	UpdateTimerText();
 }
 
 
