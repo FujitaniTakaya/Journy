b4d656ee50abec204ce0b7d0e0b16cbab42ce467
FujitaniTakaya <ca01254018@st.kawahara.ac.jp>
2025-11-22 15:14:09 +0900

    pngフォルダの移動、パープルクリスタルの追加

--- Game/Item.h
+++ Game/Item.h
@@ -1,14 +1,55 @@
 #pragma once
-class Item : public IGameObject{
+#include "Actor.h"
+
+
+class Player;
+class UI;
+
+
+enum EnItemType {
+	enItemType_PurpleCrystal = 0,
+	enItemType_Num,
+	enItemType_None = -1
+};
+
+
+
+class Item : public Actor{
+protected:
+	CollisionObject* m_collision = nullptr;
+	bool m_isEffect = false;
+
+protected:
+	Player* m_player = nullptr;
+	UI* m_ui = nullptr;
+
+
 public:
 	Item();
-	~Item();
+	virtual ~Item();
+	virtual bool Start()override;
+	virtual void Update()override;
+	virtual void Render(RenderContext& rc)override;
+
+
+protected:
+	void InitializeItem(EnItemType itemType = enItemType_Num);
+	virtual void Effect() = 0;
+	
+
+	void GetOtherClassInfo();
+};
+
+
+class PurpleCrystal : public Item {
+public:
+	PurpleCrystal();
+	~PurpleCrystal();
+	bool Start()override;
 	void Update()override;
 	void Render(RenderContext& rc)override;
 
 
 private:
-	ModelRender m_itemModelRender;
-	Quaternion m_itemRotate = Quaternion::Identity;
+	void Effect()override;
 };
-
